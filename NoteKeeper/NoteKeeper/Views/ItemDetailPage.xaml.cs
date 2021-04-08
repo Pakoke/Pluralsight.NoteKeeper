b9aa041235ccaa87e7a89f1934b1846e5e27fddb
Francisco De La Torre <Francisco.DeLaTorre@verisk.com>
2021-04-08 19:49:17 +0200

    Add project files.

---
+++ NoteKeeper/NoteKeeper/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using NoteKeeper.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace NoteKeeper.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
